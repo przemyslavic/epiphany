2667104f73cc6d547067c78b672a9cc201ccfc31
Jason Smith <388280+jsmith085@users.noreply.github.com>
2022-08-05 06:29:04 +0000

    Fix broken Epiphany 1.3 deploy by applying fixes indentified by GitHub bug

--- ansible/playbooks/roles/repository/files/download-requirements/ubuntu-20.04/download-requirements.sh
+++ ansible/playbooks/roles/repository/files/download-requirements/ubuntu-20.04/download-requirements.sh
@@ -103,6 +103,7 @@ apt update
 # parse the input file, separete by tags: [crane], [packages], [files], [images]
 crane=$(awk '/^$/ || /^#/ {next}; /\[crane\]/ {f=1; next}; /^\[/ {f=0}; f {print $0}' "${input_file}")
 packages=$(awk '/^$/ || /^#/ {next}; /\[packages\]/ {f=1; next}; /^\[/ {f=0}; f {print $0}' "${input_file}")
+packagesfromurl=$(awk '/^$/ || /^#/ {next}; /\[packagesfromurl\]/ {f=1; next}; /^\[/ {f=0}; f {print $0}' "${input_file}")
 files=$(awk '/^$/ || /^#/ {next}; /\[files\]/ {f=1; f=2; next}; /^\[/ {f=0}; f {print $0}' "${input_file}")
 images=$(awk '/^$/ || /^#/ {next}; /\[images\]/ {f=1; next}; /^\[/ {f=0}; f {print $0}' "${input_file}")
 
@@ -176,6 +177,30 @@ else
 fi
 
 printf "\n"
+
+# PACKAGES AS URL
+# process files
+
+check_connection wget $(for file in $packagesfromurl; do echo "$file"; done)
+
+
+if [[ -z "${packagesfromurl}" ]]; then
+    echol "No packages from URL to download"
+else
+    # be verbose, show what will be downloaded
+    # TODO: this is the list of all files shows on every run, not only the files that will be downloaded this run
+    echol "Packages from URL to be downloaded:"
+    cat -n <<< "${packagesfromurl}"
+
+    printf "\n"
+    # download files using wget
+    while IFS= read -r file; do
+        # download files, skip if exists
+        #wget --no-verbose --continue --directory-prefix="${dst_dir_files}" "${file}"
+        #wget --continue --show-progress --directory-prefix="${dst_dir_files}" "${file}"
+        download_file "${file}" "${dst_dir_packages}"
+    done <<< "${packagesfromurl}"
+fi
 
 check_connection wget $(for file in $files; do echo "$file"; done)
 
